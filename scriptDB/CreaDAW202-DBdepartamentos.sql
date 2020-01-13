5d049e81e0e8f90f7aa79ff9bcc64aab00138d76
Daniel <Daniel@DESKTOP-N9G2QFE>
2020-01-13 15:34:01 +0100

    CASA 13/01/2020 Inicio

---
+++ scriptDB/CreaDAW202-DBdepartamentos.sql
@@ -0,0 +1,39 @@
+/**
+ * Author:  David Garcia
+ * Created: 26-nov-2019
+ */
+-- Cambiar nombre de la base de datos y el de usuario --
+-- ProyectoTema5 || LoginLogoffTema5 || MtoDepartamentosPDOTema5 --
+
+-- Crear base de datos --
+    CREATE DATABASE if NOT EXISTS DAW202DBLoginLogoff;
+-- Uso de la base de datos. --
+    USE DAW202DBLoginLogoff;
+
+-- Crear tablas. --
+    CREATE TABLE IF NOT EXISTS Departamento(
+        CodDepartamento varchar(3) PRIMARY KEY,
+        DescDepartamento varchar(255) NOT null,
+        FechaBajaDepartamento int DEFAULT null, -- Valor por defecto null, ya que cuando lo creas no puede estar de baja logica
+        FechaCreacionDepartamento int NOT null,
+        VolumenNegocio float NOT null
+    );
+
+    CREATE TABLE IF NOT EXISTS Usuario(
+        CodUsuario varchar(15) PRIMARY KEY,
+        DescUsuario varchar(250) NOT null,
+        Password varchar(64) NOT null,
+        NumConexiones INTEGER(10) default 0,
+        Perfil enum('administrador', 'usuario') default 'usuario', -- Valor por defecto usuario
+        FechaHoraUltimaConexion timestamp, --Revisar o cambiar a int--
+        ImagenUsuario mediumblob
+    );
+
+-- Crear del usuario --
+    CREATE USER IF NOT EXISTS 'usuarioDAW202DBLoginLogoff'@'%' identified BY 'paso'; 
+
+-- Dar permisos al usuario --
+    GRANT ALL PRIVILEGES ON DAW202DBLoginLogoff.* TO 'usuarioDAW202DBLoginLogoff'@'%'; 
+
+-- Hacer el flush privileges, por si acaso --
+    FLUSH PRIVILEGES;
