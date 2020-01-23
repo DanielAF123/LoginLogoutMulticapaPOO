1c504790a0b2c8bdbc419698be4529230df02b32
Daniel <Daniel@DESKTOP-N9G2QFE>
2020-01-23 21:33:44 +0100

    CASA 23/01/2020 Borrar

--- view/vlogin.php
+++ view/vlogin.php
@@ -1,11 +1,15 @@
-<div id="formulario">
-<form name="formulario" action="../index.php" method="POST" enctype="multipart/form-data">
-        <label for="usuario">Usuario</label>
-        <input type="text" name="codUsuario" id="usuario"><br>
+<div class="formulario">
+<form name="formularioa" action="../index.php" method="POST" enctype="multipart/form-data">
+    <div>    
+    <label for="usuario">Usuario</label>
+        <input type="text" name="codUsuario" id="usuario">
         <label for="contrasena">Contrasena</label>
         <input type="password" name="password" id="contrasena">
-        <input type="submit" value="Aceptar" name="Aceptar">
+        <input class="boton margen" type="submit" value="Aceptar" name="Aceptar">
+        </div>
     </form>
-    <a href="Layout.php?pagina=registro"><button>Registrate</button></a>
-    <a href="../../index.html"><button>Salir</button></a><br>
+    <div >
+    <a href="Layout.php?pagina=registro"><button class="boton">Registrate</button></a>
+    <a href="../../index.html"><button class="boton">Salir</button></a><br>
+    </div>
 </div>
